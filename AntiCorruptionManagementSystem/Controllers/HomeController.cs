a76fac33625d952109b96c81affc78eb79a6ca13
aniksaha <pappubd007@gmail.com>
2017-12-27 15:32:24 +0600

    dashboard and logo

--- AntiCorruptionManagementSystem/Controllers/HomeController.cs
+++ AntiCorruptionManagementSystem/Controllers/HomeController.cs
@@ -1,11 +1,34 @@
-﻿using System.Web.Mvc;
+﻿using AntiCorruptionManagementSystem.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace AntiCorruptionManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        public AcmsDbContext _db = null;
+        public UserManager<ApplicationUser> UserManager = null;
+
+        public HomeController()
+        {
+            this._db = new AcmsDbContext();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new AcmsDbContext()));
+        }
+
         public ActionResult Index()
         {
+            ViewBag.TotalWing = _db.Wing.Count();
+            ViewBag.TotalSajeka = _db.Sajeka.Count();
+            ViewBag.Cases = _db.Case.Count();
+            ViewBag.CasesObjection = _db.CaseObjection.Count();
+
+            ViewBag.TotalEmployee = _db.Employee.Count();
+            ViewBag.TotalAccuser = _db.Accuser.Count();
+            ViewBag.TotalAccused = _db.AccusedPersonInfo.Count();
+            ViewBag.Time = DateTime.Now;
             return View();
         }
 
